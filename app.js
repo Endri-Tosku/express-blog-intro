4c8e5e814e3f68cef9822015a747ccec05f33a1a
Endri-Tosku <endritosku001@gmail.com>
2026-02-05 17:00:23 +0100

    feat: aggiunto arrey di post

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('Server del mio blog')
+
+    // array dei post definito all'inizio
+    const posts = [
+        { title: "Post 1", content: "Testo del post", img: "img1.jpg", tags: ["tag1"] },
+        { title: "Post 2", content: "Testo del post", img: "img2.jpg", tags: ["tag2"] },
+        { title: "Post 3", content: "Testo del post", img: "img3.jpg", tags: ["tag3"] },
+        { title: "Post 4", content: "Testo del post", img: "img4.jpg", tags: ["tag4"] },
+        { title: "Post 5", content: "Testo del post", img: "img5.jpg", tags: ["tag5"] },
+    ]
+    res.json(posts)
 })
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
